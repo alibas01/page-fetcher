084336e6a9667eb99d7137871321025619aef3df
Ali Bas <alibas01@gmail.com>
2021-01-14 13:57:59 -0500

    files created

---
+++ fetcher.js
@@ -0,0 +1,23 @@
+let arg = process.argv;
+// arg = arg.slice(2);
+
+
+const request = require('request');
+const { fileURLToPath } = require('url');
+const fs = require('fs');
+
+const URL = arg[2];
+const filePath = arg[3];
+
+
+request(URL, (error, response, body) => {
+  fs.writeFile(filePath, `ERROR: ${error} \n\n-------------\n statusCode: ${response && response.statusCode}\n\n-------------\n body:${body}`, function (err) {
+    if (err) throw err;
+    
+    console.log(`Downloaded and saved ${body.length} bytes to ./result.txt`);
+    
+  });
+}); 
+
+
+
